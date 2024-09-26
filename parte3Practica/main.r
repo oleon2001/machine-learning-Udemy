8cbf37660afe271adb18e3287f96358b378954eb
oleon2001 <oswaldoleon72@gmail.com>
2024-09-26 16:29:52 -0400

    aaaaaaaaaaaa

--- parte3Practica/main.r
+++ parte3Practica/main.r
@@ -1,12 +1,22 @@
-#plantilla de preprocesado de datos
+#regresion lineal multiple
 
-dataset=read.csv('~/Escritorio/Udemy-Machine_learning/original/Part 2 - Regression/Section 4 - Simple Linear Regression/Salary_Data.csv')
+dataset=read.csv('~/Escritorio/Udemy-Machine_learning/original/Part 2 - Regression/Section 5 - Multiple Linear Regression/50_Startups.csv')
+
+#codificar las variables categoricas
+#c es para trabajar con vectorews
+dataset$State = factor(dataset$State,
+                         levels = c('New York','California', 'Florida'),
+                         labels = c(1,2,3))
 
 
 #install.packages("caTools")
 #dividir los datos en conjunto de entrenamiento y conjunto de prueba 
 library(caTools)
 set.seed(123)
-split=sample.split(dataset$Salary, SplitRatio = 2/3)
+split=sample.split(dataset$Profit, SplitRatio = 0.8)
 training_set = subset(dataset, split==TRUE)
 testing_Set = subset(dataset, split==FALSE)
+
+
+#ajustar el modelo de regresion lineal multiple con el conjunto de entrenamiento 
+regression = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State)
